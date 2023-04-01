e8f5e6906ca6c0bad80e1bfa48f1a0c0b37d7bd8
Mickhat <25436056+Mickhat@users.noreply.github.com>
2023-04-01 15:14:20 +0200

    fixed a typo  "Chagning_hostname" to "Chaning_hostname"
    fixed a typo  "Chagning_hostname" to "Chaning_hostname"

--- automation_scripts/Change-Hostname.ps1
+++ automation_scripts/Change-Hostname.ps1
@@ -4,7 +4,7 @@ param
     [string]$user = "Administrator",
     [string]$hostname = "localhost_1"
 )
-Write-Host -ForegroundColor Green "Chagning_hostname"
+Write-Host -ForegroundColor Green "Changing_hostname"
 $pass=ConvertTo-SecureString $password -AsPlainText -Force
 $cred=New-Object System.Management.Automation.PSCredential ($user, $pass)
 Rename-Computer -NewName $hostname -DomainCredential $cred
